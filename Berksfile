926ee72c87831198ca371fc2d0762775687a9fc9
Josh Guice <jguice@gmail.com>
2015-10-08 10:22:17 -0700

    fix(berks): metadata problems

--- Berksfile
+++ Berksfile
@@ -2,7 +2,7 @@ source 'https://supermarket.chef.io'
 
 metadata
 
-cookbook 'repose', git: 'https://github.com/rackerlabs/cookbook-repose.git'
-cookbook 'minitest-handler'
 cookbook 'apt'
 cookbook 'java'
+cookbook 'repose', git: 'https://github.com/rackerlabs/cookbook-repose.git'
+cookbook 'minitest-handler'
--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,5 @@ license          'Apache 2.0'
 description      'Installs/Configures wrapper-repose'
 long_description 'Installs/Configures wrapper-repose'
 version          '0.1.0'
-depends          'repose'
-depends          'apt'
-depends          'java'
+
+# NOTE currently defined in Berksfile...defining here and loading via Berksfile not quite working :(

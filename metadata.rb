a8aa98125f6cc811693309807423543f742dd216
Josh Guice <jguice@gmail.com>
2015-10-10 09:19:17 -0700

    chore(version): bump

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ maintainer_email 'YOUR_EMAIL'
 license          'Apache 2.0'
 description      'Installs/Configures wrapper-repose'
 long_description 'Installs/Configures wrapper-repose'
-version          '0.1.0'
+version          '0.1.1'
 
 # NOTE currently defined in Berksfile...defining here and loading via Berksfile not quite working :(

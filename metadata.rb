5eeaba4b85a9144b74bb6fc1a0c52e4933bb4c8d
Michael Burns <michael.burns@rackspace.com>
2015-10-12 21:29:44 -0700

    version bump

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'sfo-devops@lists.rackspace.com'
 license 'Apache 2.0'
 description 'Installs/Configures wrapper-repose'
 long_description 'Installs/Configures wrapper-repose'
-version '0.2.0'
+version '0.2.1'
 
 depends 'apt'
 depends 'java'

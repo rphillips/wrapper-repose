fc9d09e19ce3196ceed1f155c12ac36092ff5f3e
Josh Guice <jguice@gmail.com>
2015-10-13 08:13:08 -0700

    chore(ver/todo): bump/remove

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,10 @@
-# TODO: rename to maas-repose?
 name 'wrapper-repose'
 maintainer 'Rackspace'
 maintainer_email 'sfo-devops@lists.rackspace.com'
 license 'Apache 2.0'
 description 'Installs/Configures wrapper-repose'
 long_description 'Installs/Configures wrapper-repose'
-version '0.2.1'
+version '0.2.2'
 
 depends 'apt'
 depends 'java'

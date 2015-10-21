383a8e859e15836561ba6915083d4779d719fa1e
Tim Boudreau <tim.boudreau@rackspace.com>
2015-10-20 22:14:56 -0700

    Pre-authorize hybridRole in Valkyrie filter.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sfo-devops@lists.rackspace.com'
 license 'Apache 2.0'
 description 'Installs/Configures wrapper-repose'
 long_description 'Installs/Configures wrapper-repose'
-version '0.2.10'
+version '0.2.11'
 
 depends 'apt'
 depends 'java'
